9277ade6a3177f48ce5d2b72dc4e4ae1edadc074
Miliexe <emilie.lblc@gmail.com>
2022-10-07 00:45:11 +0200

    Link to Database

---
+++ back/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//const Sauce = require('./models/sauces');
+
+mongoose.connect('mongodb+srv://Miliexe:exrnm1nDsN8rQy2q@cluster0.gcnsiid.mongodb.net/sauces?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+module.exports = app;
