f7cbb4fc88460f8cfc9fd4eb9b362fcaec30020c
Miliexe <emilie.lblc@gmail.com>
2022-10-18 23:49:14 +0200

    Fix Unlink images

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -43,11 +43,19 @@ exports.modifySauce = (req, res, next) => {
   delete sauceObject._userId;
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
+      const filename = sauce.imageUrl.split('/images/')[1];
       if (sauce.userId != req.auth.userId) {
         res.status(401).json({ message: 'Not authorized' });
       } else {
         Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+          .then(() => {
+            if (req.file != undefined) {
+              fs.unlink(`images/${filename}`, (error) => {
+                  if (error) throw error
+              })
+          }
+            res.status(200).json({ message: 'Objet modifié!' })
+          })
           .catch(error => res.status(401).json({ error }));
       }
     })
